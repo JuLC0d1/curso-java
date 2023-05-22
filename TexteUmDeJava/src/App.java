e714ca0d852537d8afebd876293ec68fdd8568e7
Juliana <jd391829@gmail.com>
2023-05-22 17:53:17 -0300

    inicio

---
+++ TexteUmDeJava/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+/*E É ASSIM QUE SE FAZ A PRIMEIRA MENSAGEM APARECER EM JAVA, MUITO GRANDE EU SEI, MAS FAZER OQ*/
